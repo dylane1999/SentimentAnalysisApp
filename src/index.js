e6fc24c31736490984789eb23dc78417126a28e0
Dylan Edwards <dylanedwards290@gmail.com>
2020-11-30 00:58:37 -0800

    hit api with proxy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+window.axios = axios;
 
 ReactDOM.render(
   <React.StrictMode>
