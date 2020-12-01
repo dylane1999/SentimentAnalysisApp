c856a307849acc5208f82eb14d21e412d6e7968e
Dylan Edwards <dylanedwards290@gmail.com>
2020-12-01 11:09:03 -0800

    fix target

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    ['/api'],
     createProxyMiddleware({
-      target: 'https://randomuser.me/api',
+      target: 'https://randomuser.me',
       changeOrigin: true,
     })
   );
