7bc548cded554b565c46b4ff0f0055932934ec0c
Dylan Edwards <dylanedwards290@gmail.com>
2020-12-18 18:31:25 -0800

    change backend address back to docker link

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/analyze',
     createProxyMiddleware({
-      target: `http://localhost:5000`,
+      target: `http://backend:5000`,
       changeOrigin: true,
     })
   );
